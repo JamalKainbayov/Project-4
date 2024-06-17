e1a1ff9dc0031f8c5095d66e07bffe5e0fde83d1
Frog1810 <Frogblaauw@gmail.com>
2024-06-17 12:01:51 +0200

    doe ding

--- wordpress/wp-content/themes/Project-4/index.php
+++ wordpress/wp-content/themes/Project-4/index.php
@@ -6,13 +6,10 @@
 </body>
 <footer>
 <?php get_footer()  ?>
-<<<<<<< HEAD
 <img src="<?php echo esc_html( get_field('slider-2') ); ?>" alt="hi">
 </html>
 
-=======
 </footer>
->>>>>>> a98f569431698ca254e1a3ead0eae1831bb33bef
 <style>
     <?php include 'style.css'; ?>
 </style>
