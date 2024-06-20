d829df6f6ef655ef59f52d16e090349f58404223
Jamal Kainbayov <9023725@student.zadkine.nl>
2024-06-20 09:41:43 +0200

    css added

--- wordpress/wp-content/themes/Project-4/rick-template.php
+++ wordpress/wp-content/themes/Project-4/rick-template.php
@@ -1,6 +1,13 @@
 <?php /* Template Name: Rick Template */ ?>
-
+<?php get_header()?>
+<?php include 'nav.php'?>
+<body>
+<h1><?php echo get_the_title()?></h1>
+<?php echo get_the_content()?>
 <div class="Sldiers">
     <img src="<?php echo esc_html( get_field('slider-2') ); ?>" alt="hi">
     <img src="<?php echo esc_html( get_field('slider-1') ); ?>" alt="hi1">
 </div>
+<style>
+    <?php include 'style.css'; ?>
+</style>
