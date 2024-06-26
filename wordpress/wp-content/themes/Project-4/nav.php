cb610b9e687836dbb3aa5389d087f3ea9ca43dcd
Jamal Kainbayov <9023725@student.zadkine.nl>
2024-06-26 12:07:46 +0200

    Jamal UIT v2

--- wordpress/wp-content/themes/Project-4/nav.php
+++ wordpress/wp-content/themes/Project-4/nav.php
@@ -2,7 +2,7 @@
     <div ><a class="artist-name" href="http://localhost/Project-4/wordpress/">Niek Hendrix</a></div>
     <ul>
         <li><a class="hover" href="#">Paintings</a></li>
-        <li><a class="hover"  href="#">Biography</a></li>
+        <li><a class="hover"  href="http://localhost/Project-4/wordpress/biography/">Biography</a></li>
         <li><a class="hover"  href="http://localhost/Project-4/wordpress/artist-statement/">Artist Statement</a></li>
         <li><a class="hover"  href="http://localhost/Project-4/wordpress/media/">Media</a></li>
         <li><a class="Contact" href="http://localhost/Project-4/wordpress/contact/">Contact</a></li>
